ab9913c646b8db8ffe2627e3623bdbfb6a22d0ea
Michelle Yenny <mmy491@gmail.com>
2018-12-02 22:30:43 -0500

    input boxes working

--- src/App.js
+++ src/App.js
@@ -1,28 +1,55 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      artist: '',
+      songName: ''
+    }
+  }
+
+  changeArtistName = event => {
+    this.setState({
+      artist: event.target.value
+    })
+  }
+
+  changeSongName = event => {
+    this.setState({
+      songName: event.target.value
+    })
+  }
+
+  getSongLyrics = event => {
+    event.preventDefault()
+    console.log(this.state.songName)
+    console.log(this.state.artist)
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>Lyrics</h1>
+        <form onSubmit={this.getSongLyrics}>
+          <input
+            type="text"
+            placeholder="Artist"
+            onChange={this.changeArtistName}
+          />
+        </form>
+        <form onSubmit={this.getSongLyrics}>
+          <input
+            type="text"
+            placeholder="Title of Song"
+            onChange={this.changeSongName}
+          />
+        </form>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
